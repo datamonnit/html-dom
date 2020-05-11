d70ad647b4b677c9a4ccb165d8971be8259d022e
Tuomas Puro <tuomas.puro@esedu.fi>
2020-05-11 10:43:22 +0300

    sulut unohtui

--- part4/dom.js
+++ part4/dom.js
@@ -48,7 +48,7 @@ function filterItems(e){
     Array.from(items).forEach(function(item){
         let itemName = item.firstChild.textContent;
         
-        if (itemName.toLowerCase.indexOf(text) != -1) {
+        if (itemName.toLowerCase().indexOf(text) != -1) {
             item.style.display = 'block';
         } else {
             item.style.display = 'none';
